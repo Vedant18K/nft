f1a4435ecb04b0692a0181c131e0461c6c279918
Vedant18K <115392507+Vedant18K@users.noreply.github.com>
2023-11-09 16:16:57 +0530

    Add Google Maps in Contact pages

--- src/ContactUs/Contact.jsx
+++ src/ContactUs/Contact.jsx
@@ -1,64 +1,70 @@
-import React from "react";
-import NavBar from "../HomePage/NavBar";
-import sideimg from "../Images/contact1.webp"
+  import React from "react";
+  import NavBar from "../HomePage/NavBar";
+  import Footer from "../HomePage/Footer";
+  import sideimg from "../Images/contact1.webp"
+import MapComponent from "./MapComponent";
 
-const Contact = () => {
-  return (
-    <div>
+  const Contact = () => {
+    return (
       <div>
-        <NavBar />
-        <div className="pt-[100px] text-center">
-          <div>Quick Contact Address</div>
-          <div>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration.
-          </div>
-        </div>
-        <div className="flex justify-evenly items-center m-10">
-          <div className="border shadow-xl p-[80px]">
-            <div className="text-[20px] font-bold pb-8">Contact Phone Number</div>
-            <div>+444 555 666 777</div>
-            <div>+444 555 666 777</div>
-          </div>
-          <div className="border shadow-xl p-[80px]">
-            <div className="text-[20px] font-bold pb-8">Our Email Address</div>
-            <div>admin@gmail.com</div>
-            <div>example@gmail.com</div>
-          </div>
-          <div className="border shadow-xl p-[80px]">
-            <div className="text-[20px] font-bold pb-8">Our Location</div>
-            <div>5678 Bangla Main Road, cities 580</div>
-            <div>GBnagla, example 54786</div>
-          </div>
-        </div>
-        <div className="flex justify-between px-5">
-            <div ><img src={sideimg} alt="contactus"/></div>
-            <div className="border w-[50%]">
-                <div className="text-[24px] font-bold ">Contact Us</div>
-                <div>
-                    <div>Your Name</div>
-                    <div className=""><input /></div>
-                </div>
-                <div>
-                    <div>Email</div>
-                    <div><input /></div>
-                </div>
-                <div>
-                    <div>Subject</div>
-                    <div><input /></div>
-                </div>
-                <div>
-                    <div>Write Message</div>
-                    <div><input /></div>
-                </div>
-                <div>
-                    <button>Send Message</button>
-                </div>
+        <div>
+          <NavBar />
+          <div className="pt-[100px] text-center">
+            <div>Quick Contact Address</div>
+            <div>
+              There are many variations of passages of Lorem Ipsum available, but
+              the majority have suffered alteration.
             </div>
+          </div>
+          <div className="flex justify-evenly items-center m-10">
+            <div className="border shadow-xl p-[80px]">
+              <div className="text-[20px] font-bold pb-8">Contact Phone Number</div>
+              <div>+444 555 666 777</div>
+              <div>+444 555 666 777</div>
+            </div>
+            <div className="border shadow-xl p-[80px]">
+              <div className="text-[20px] font-bold pb-8">Our Email Address</div>
+              <div>admin@gmail.com</div>
+              <div>example@gmail.com</div>
+            </div>
+            <div className="border shadow-xl p-[80px]">
+              <div className="text-[20px] font-bold pb-8">Our Location</div>
+              <div>5678 Bangla Main Road, cities 580</div>
+              <div>GBnagla, example 54786</div>
+            </div>
+          </div>  
+          <div className="flex justify-between px-5 mb-5">
+              <div className="flex justify-center p-5 border rounded-md shadow-md" ><img  className="rounded-lg hover:translate-x-1" src={sideimg} alt="contactus"/></div>
+              <div className="border rounded-md w-[50%] p-[80px]">
+                  <div className="text-[24px] font-bold ">Contact Us</div>
+                  <div className="pt-3">
+                      <div className="mb-2">Your Name</div>
+                      <div ><input className="border rounded-md shadow-md w-[400px] h-[40px]"/></div>
+                  </div>
+                  <div className="pt-3">
+                      <div className="mb-2">Email</div>
+                      <div ><input className="border rounded-md shadow-md w-[400px] h-[40px]"/></div>
+                  </div>
+                  <div className="pt-3">
+                      <div className="mb-2">Subject</div>
+                      <div><input className="border rounded-md shadow-md w-[400px] h-[40px]" /></div>
+                  </div>
+                  <div className="pt-3">
+                      <div className="mb-2">Write Message</div>
+                      <div ><textarea className="border w-[400px] shadow-md h-[100px]"/></div>
+                  </div>
+                  <div className="pt-3">
+                      <button className="border p-4 rounded-md bg-[#00a3ff] text-white hover:bg-black ">Send Message</button>
+                  </div>
+              </div>
+          </div>
+          <MapComponent/>
+          <Footer/>
         </div>
+        
       </div>
-    </div>
-  );
-};
+      
+    );
+  };
 
-export default Contact;
+  export default Contact;
---
+++ src/ContactUs/MapComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
+export default function MapComponent() {
+  const params = new URLSearchParams({
+    q: 'Berlin, Germany', // Specify the location by name
+    hl: 'en', // Specify English as the language
+  });
+
+  return (
+    <MDBRow className='w-100'>
+      <MDBCol className='my-2'>
+        <iframe
+          src={`https://www.google.com/maps/embed?${params.toString()}`}
+          className='w-full'
+          height='400'
+          loading='lazy'
+          title='Google Map of Berlin, Germany'
+          allowFullScreen=''
+          frameBorder='0'
+          style={{ border: '0',  }} // Adjust styling here
+        ></iframe>
+      </MDBCol>
+    </MDBRow>
+  );
+}
